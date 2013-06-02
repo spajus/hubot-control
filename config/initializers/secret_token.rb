102b0d09503ac4b6d4a92cf755e90027bbc52d42
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2013-06-02 13:48:49 +0300

    Adding freshly generated rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HubotControl::Application.config.secret_key_base = '187d38aeaa2e3ef9296ffcf7a8373c9e5fb7dfff44278cdd290b43f1a55021274f434304a96bc076c11f967e1bb1886d4d5154c8591e16a5e59e843e532cafb0'
