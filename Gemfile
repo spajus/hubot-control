b8bfa70e0c458c7f88fbb947a586609176fe778a
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2013-06-02 19:04:22 +0300

    Integrating Twitter Bootstrap

--- Gemfile
+++ Gemfile
@@ -15,8 +15,9 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
 
-# See https://github.com/sstephenson/execjs#readme for more supported runtimes
-# gem 'therubyracer', platforms: :ruby
+gem 'therubyracer', '~> 0.11'
+gem 'less-rails', '~> 2.3'
+gem 'twitter-bootstrap-rails', '~> 2.2'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 HubotControl::Application.routes.draw do
+  get "dashboard/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'dashboard#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
