9d8c4f4d5d0fcaf8cdf93515e4e4b21e728bcb20
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2013-06-02 22:26:37 +0300

    Some stats about current installation

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,4 +1,10 @@
 class StatusController < ApplicationController
   def show
+    @node   = `node -v`
+    @npm    = `npm -v`
+    @coffee = `coffee -v`
+    @hubot  = `hubot -v`
+    @hubots_dir = Rails.root.join('hubots')
+    @hubots_dir_perms = File.writable? @hubots_dir
   end
 end
