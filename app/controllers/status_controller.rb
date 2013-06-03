85e998383784730b746c0e3b169c077d66d303d9
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2013-06-03 06:51:48 +0300

    Improving status checks

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -8,5 +8,6 @@ class StatusController < ApplicationController
     @hubots_dir       = Hubot.base_dir
     @hubots_dir_perms = Status.hubot_dir_writable?
     @sys_user         = Status.sys_user
+    @not_root         = Status.not_root?
   end
 end
--- app/models/status.rb
+++ app/models/status.rb
@@ -3,19 +3,19 @@ class Status
   class << self
 
     def node
-      @@node ||= `node -v`
+      @@node ||= `node -v`.try(:strip)
     end
 
     def npm
-      @@npm ||= `npm -v`
+      @@npm ||= `npm -v`.try(:strip)
     end
 
     def coffee
-      @@coffee ||= `coffee -v`
+      @@coffee ||= `coffee -v`.try(:strip)
     end
 
     def hubot
-      @@hubot ||= `hubot -v`
+      @@hubot ||= `hubot -v`.try(:strip)
     end
 
     def hubot_dir_writable?
@@ -23,11 +23,15 @@ class Status
     end
 
     def sys_user
-      @@sys_user ||= `whoami`
+      @@sys_user ||= `whoami`.try(:strip)
+    end
+
+    def not_root?
+      self.sys_user != 'root'
     end
 
     def can_run?
-      self.node && self.npm && self.coffee && self.hubot_dir_writable?
+      self.node && self.npm && self.coffee && self.hubot_dir_writable? && self.not_root?
     end
 
   end
