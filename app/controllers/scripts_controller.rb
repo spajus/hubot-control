84995d1331066ac17e267165170b8ead60bedebf
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2013-07-28 07:58:20 +0300

    Refactor scripts controller to be more readable

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -4,34 +4,18 @@ class ScriptsController < ApplicationController
   end
 
   def create
-    redirect_to edit_scripts_path(script: params[:filename])
+    redirect_to_edit
   end
 
   def edit
-
-    filename = params[:script]
-
-    unless valid_filename?(filename)
+    unless valid_filename?(params[:script])
       flash[:error] = "Unsupported script name or type"
       return redirect_to scripts_path
     end
 
-    @script = Script.new(filename)
-    if request.post?
+    @script = Script.new(params[:script])
 
-      if params[:filename] && params[:filename] != filename
-        if valid_filename?(params[:filename])
-          @script.delete
-          @script.filename = params[:filename]
-          redirect_to edit_scripts_path(params[:filename])
-        else
-          flash[:error] = "Filename should end with .js or .coffee"
-        end
-      end
-
-      @script.write params[:content]
-      flash[:success] = 'Script updated'
-    end
+    update_script
   end
 
   def destroy
@@ -42,6 +26,30 @@ class ScriptsController < ApplicationController
   end
 
   private
+
+    def update_script
+      return unless request.post?
+
+      handle_rename
+
+      @script.write params[:content]
+      flash[:success] = 'Script updated'
+    end
+
+    def handle_rename
+      return unless params[:filename] && params[:filename] != params[:script]
+      unless valid_filename?(params[:filename])
+        flash[:error] = "Filename should end with .js or .coffee"
+        return
+      end
+      @script.delete
+      @script.filename = params[:filename]
+      redirect_to_edit
+    end
+
+    def redirect_to_edit
+      redirect_to edit_scripts_path(script: params[:filename])
+    end
 
     def valid_filename?(filename)
       filename.match(/^[a-z0-9_-]+\.(js|coffee)$/i)
