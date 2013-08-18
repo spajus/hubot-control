8c6d8b55a5262c3378f0c0d6dadf92ca82332c75
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2013-08-18 14:36:42 +0300

    Add specs for creating / updating Hubot

--- spec/controllers/hubots_controller_spec.rb
+++ spec/controllers/hubots_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe HubotsController do
 
   let(:user) { create :user }
+  let(:hubot) { create :hubot }
   before { sign_in :user, user }
 
   describe 'GET #index' do
@@ -15,5 +16,36 @@ describe HubotsController do
     subject { get :new }
 
     it { should be_success }
+  end
+
+  describe 'GET #show' do
+    subject { get :show, id: hubot.id }
+
+    it { should be_success }
+  end
+
+  describe 'POST #create' do
+    let(:hubot_params) { {
+      title: 'New Hubot',
+      name: 'new_hubot',
+      adapter: 'shell',
+      port: 10101,
+      test_port: 10102,
+    }}
+    subject { post :create, hubot: hubot_params }
+
+    specify do
+      expect { subject }.to change { Hubot.count }.by(1)
+    end
+  end
+
+  describe 'PATCH #update' do
+    let(:old_title) { hubot.title }
+    let(:new_title) { 'New Hubot Title' }
+    subject { patch :update, id: hubot.id, hubot: { title: new_title } }
+
+    specify do
+      expect { subject }.to change { hubot.reload.title }.from(old_title).to(new_title)
+    end
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,11 @@ FactoryGirl.define do
     password 'testuser123'
   end
 
+  factory :hubot do
+    sequence(:name) { |n| "hubot#{n}" }
+    sequence(:title) { |n| "Hubot ##{n}" }
+    sequence(:port) { |n| 8000 + n }
+    sequence(:test_port) { |n| 9000 + n }
+    adapter 'shell'
+  end
 end
